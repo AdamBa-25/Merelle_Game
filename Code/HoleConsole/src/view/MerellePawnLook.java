346bcb5bbcb55c0fc86c191799bba194cd26e2a1
lue <lukas.bendiaf@edu.univ-fcomte.fr>
2026-05-20 20:08:18 +0200

    ca aussi cest fait

--- Code/HoleConsole/src/view/MerellePawnLook.java
+++ Code/HoleConsole/src/view/MerellePawnLook.java
@@ -7,26 +7,28 @@ import model.MerellePawn;
 
 /**
  * Look console d'un pion de la Mérelle.
- *
- * Affiche 1 caractère coloré :
- *  - Joueur Noir : fond noir, lettre 'N' blanche
- *  - Joueur Rouge : fond rouge, lettre 'R' noire
- *
- * Calqué sur PawnLook.java du tutoriel HoleConsole.
+
+ * Affiche un seul caractère coloré dans shape[0][0] :
+ *   couleur texte + couleur fond + lettre du pion + RESET
+
+ * Ce look sert pour les pions en réserve (pas encore posés sur le plateau).
+ * Les pions posés sont dessinés directement par BoardLook dans MerelleStageView.
  */
 public class MerellePawnLook extends ElementLook {
 
-    /**
-     * @param element le MerellePawn associé à ce look
-     */
     public MerellePawnLook(GameElement element) {
+
         super(element, 1, 1);
     }
 
     @Override
-    protected void render()
-    {
+    protected void render() {
+
         MerellePawn pawn = (MerellePawn) element;
-        shape[0][0] = pawn.getTextColor() + pawn.getBackgroundColor() + pawn.getSymbol() + ConsoleColor.RESET;
+
+        shape[0][0] = pawn.getTextColor()
+                + pawn.getBackgroundColor()
+                + pawn.getSymbol()
+                + ConsoleColor.RESET;
     }
 }
